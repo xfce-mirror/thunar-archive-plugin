a09ca70346e145cf069d101a40b874d9e68d6fd0
Andre Miranda <andre42m@gmail.com>
2017-05-17 23:13:21 -0300

    Use gtk_dialog_get_content_area instead of directly accessing private vbox

--- thunar-archive-plugin/tap-backend.c
+++ thunar-archive-plugin/tap-backend.c
@@ -87,7 +87,7 @@ tap_backend_mime_ask (GList     *mime_applications,
   gtk_table_set_col_spacings (GTK_TABLE (table), 12);
   gtk_table_set_row_spacings (GTK_TABLE (table), 6);
   gtk_container_set_border_width (GTK_CONTAINER (table), 6);
-  gtk_box_pack_start (GTK_BOX (GTK_DIALOG (dialog)->vbox), table, TRUE, TRUE, 0);
+  gtk_box_pack_start (GTK_BOX (gtk_dialog_get_content_area (GTK_DIALOG (dialog))), table, TRUE, TRUE, 0);
   gtk_widget_show (table);
 
   /* add the header image */
